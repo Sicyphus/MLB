1ec0277967cdb08b9ecce6777d21b40bfaaffa6d
Sicyphus <jhturner900@gmail.com>
2023-06-03 15:08:52 -0400

    still working on transition to RG data

--- code_for_Github.py
+++ code_for_Github.py
@@ -1,4 +1,4 @@
-#python code_for_Github.py  4_26 9 4 3 # gets data for 4_26, 9 games, at most 4-stack, budget 50K
+#python code_for_Github.py 4_26 9 3     # April 26, 9 games, max overlap 3
 import pandas as pd, numpy as np, sys, random, scipy.sparse as sp, time, solver, os
 
 
@@ -8,68 +8,47 @@ def col_to_npbool(dframe, col, val):
     return vec
 
 def grader(df, sol_matrix, basefilename):  # score results\output to file
-    filename = 'results/{}.csv'.format('_'.join(basefilename))
+    filename = 'out/{}.csv'.format('_'.join(basefilename))
     scorecum = 0
     with open(filename, 'w') as f:
         for sol in sol_matrix :
-            score = df['Actual FP'].dot(sol)
+            score = df['FPTS'].dot(sol)
             scorecum += score
             f.write(str(score)+',')
         f.write(str(scorecum))
 
 def team_sampler(dframe, n): # get sample of 2n teams from team list
-    allteams = set(dframe['Team_x'].to_list())
+    allteams = set(dframe['TEAM_x'].to_list())
     if len(allteams)/2 < n:
         print('Not enough games for total teams')
         sys.exit()
     selecteams = random.sample(allteams, n)  # select 2n teams
     opps = []
     for team in selecteams:  # for each team, find opponent and add to list
-        opp = dframe[dframe['Team_x'] == team]['Opp_x'].to_list()
+        opp = dframe[dframe['TEAM_x'] == team]['OPP_x'].to_list()
         if len(set(opp)) > 1: print('Warning: Opponent team ambiguity')
         else: opps.append(opp[0])
     #print(selecteams+opps)
     return selecteams+opps
 
-def team_fromfile(date):
-    df1 = pd.read_csv('platform/DKSalaries{}.csv'.format(date))
-    df2 = pd.read_csv('platform/FDSalaries{}.csv'.format(date))
-    allteams1 = list(set(df1['TeamAbbrev'].to_list()))
-    allteams2 = list(set(df2['Team'].to_list()))
-    team_trans = {'WSH':'WAS'}
-    for i in range(len(allteams1)): 
-        if allteams1[i] in team_trans.keys(): allteams1[i] = team_trans[allteams1[i]]
-    for i in range(len(allteams2)): 
-        if allteams2[i] in team_trans.keys(): allteams2[i] = team_trans[allteams2[i]]
-    if set(allteams1) != set(allteams2):
-        print('Error: Team slate for DK/FD not same:')
-        print(set(allteams1))
-        print(set(allteams2))
-        time.sleep(4)
-    return allteams1
-    
-def name_discr(h1, h2, p1, p2, h, p): #find name discrepancies b/t FD and DK
-    s1=set(h1['Name_Team'])|set(p1['Name_Team'])
-    s2=set(h2['Name_Team'])|set(p2['Name_Team'])
-    s=set(h['Name_Team'])|set(p['Name_Team'])
-    diff1, diff2 = s1.difference(s), s2.difference(s)
-    if len(diff1) != 0 or len(diff2) != 0: 
-        print('Warning: Name Discrepancy')
-        print(diff1)
-        print(diff2)
-        time.sleep(10)
+def name_discr(dflist): #find name/team discrepancies b/t FD and DK
+    for i in range(int(len(dflist)/2)):
+        print(column_set_diff(dflist[2*i], dflist[2*i+1], 'PLAYERTEAM'))
+    time.sleep(5)
 
+def pos_split(strng):
+    if 'OF' in strng: return 'OF'
+    return strng.split('/')[0]
+    
 def printframe(dframe, cols):    
+    print('DK/FD Disparities')
     for i in range(len(dframe)):
         print(dframe.iloc[i][cols])
 
-def name_proc(frm, namelbl, teamlbl): # standardize the names (...and FD names replaced with DK)
-    frm_col = frm[namelbl].str.lower() + '_' + frm[teamlbl] 
-    bef = [' jr',' sr',r'\s+',r'\.','WSH','michaeltaylor_MIN','enriquehernandez_BOS', 'giovannyurshela_LAA', 'michaelbrosseau_MIL',  'koudaisenga_NYM', 'julioerodriguez_SEA', 'peteralonso_NYM', 'yulieskigurriel_MIA', 'ji-hwanbae_PIT','danvogelbach_NYM','abrahamtoro-hernandez_MIL']
-    aft = [r'',r'',r'',r'','WAS','michaelataylor_MIN', 'kikehernandez_BOS', 'giourshela_LAA', 'mikebrosseau_MIL', 'kodaisenga_NYM', 'juliorodriguez_SEA', 'petealonso_NYM', 'yuligurriel_MIA', 'jihwanbae_PIT','danielvogelbach_NYM','abrahamtoro_MIL']
-    for i in range(len(bef)): 
-        frm_col.replace(regex=True, inplace=True, to_replace=bef[i], value=aft[i])
-    return frm_col
+def column_set_diff(dkfrm, fdfrm, col): # find diff b/t FD/DK names/teams sets 
+    dkset = set(dkfrm[col].to_list())
+    fdset = set(fdfrm[col].to_list()) 
+    return [fdset-dkset, fdset-dkset]
 
 def create_base(nparr):  # create basis vector from positions
     tmplst = []
@@ -78,90 +57,78 @@ def create_base(nparr):  # create basis vector from positions
     return tmplst
     
 def rename(df, app):  # rename columns from merging protocol
-    return df.rename({'Player Name_'+app: 'Player Name','Team_'+app: 'Team', 'Opp_'+app: 'Opp',
-    'Pos_'+app: 'Pos','Salary_'+app: 'Salary', 'Batting Order_'+app: 'Batting Order',
-    'Proj FP_'+app: 'Proj FP','Actual FP_'+app: 'Actual FP'}, axis=1)
+    return df.rename({'PLAYER_'+app: 'PLAYER','TEAM_'+app: 'TEAM', 
+                      'OPP_'+app: 'OPP','POS_'+app: 'POS','SALARY_'+app: 'SALARY',                       
+                      'BO_'+app: 'BO','FPTS_'+app: 'FPTS'}, axis=1)
     
 def frame_maker(date, numg): # format data frames, cut away fat
     #need to process DK and FD frames separately at first
-    pdf1 = pd.read_csv('data/DFN MLB Pitchers DK {}.csv'.format(date))  #pitching
-    pdf1 = pdf1[['Player Name','Pos','Salary','Team','Opp','Proj FP','Actual FP']]
-    pdf2 = pd.read_csv('data/DFN MLB Pitchers FD {}.csv'.format(date))
-    pdf2 = pdf2[['Player Name','Pos','Salary','Team','Opp','Proj FP','Actual FP']]
-
-    hdf1 = pd.read_csv('data/DFN MLB Hitters DK {}.csv'.format(date)) #hitting (DK)
-    hdf1 = hdf1[['Player Name','Pos','Salary','Team','Opp','Batting Order (Projected)','Batting Order (Confirmed)','Proj FP','Actual FP']]#combine proj ord w/ confirmed
-    maskdkp = hdf1['Batting Order (Projected)'].isin([str(i+1) for i in range(9)])
-    hdf1.loc[~maskdkp, 'Batting Order (Projected)'] = '0' 
-    maskdkc = hdf1['Batting Order (Confirmed)'].isin([str(i+1) for i in range(9)])
-    hdf1.loc[~maskdkc, 'Batting Order (Confirmed)'] = '0' 
-    hdf1['Batting Order'] = hdf1[['Batting Order (Projected)','Batting Order (Confirmed)']].max(axis=1)    
-    hdf1.drop(['Batting Order (Confirmed)', 'Batting Order (Projected)'], axis=1,inplace=True)
-   
-    hdf2 = pd.read_csv('data/DFN MLB Hitters FD {}.csv'.format(date)) #hitting (FD)
-    hdf2 = hdf2[['Player Name','Pos','Salary','Team','Opp','Batting Order (Projected)','Batting Order (Confirmed)','Proj FP','Actual FP']]#combine proj ord w/ confirmed
-    maskfdp = hdf2['Batting Order (Projected)'].isin([str(i+1) for i in range(9)])
-    hdf2.loc[~maskfdp, 'Batting Order (Projected)'] = '0' 
-    maskfdc = hdf2['Batting Order (Confirmed)'].isin([str(i+1) for i in range(9)])
-    hdf2.loc[~maskfdc, 'Batting Order (Confirmed)'] = '0' 
-    hdf2['Batting Order'] = hdf2[['Batting Order (Projected)','Batting Order (Confirmed)']].max(axis=1)    
-    hdf2.drop(['Batting Order (Confirmed)', 'Batting Order (Projected)'], axis=1,inplace=True)
+    df_dk = pd.read_csv('in/DK{}.csv'.format(date))  #pitching
+    df_dk = df_dk[['PLAYER','PARTNERID','POS','SALARY','TEAM','OPP','FPTS']]
+    df_dk['PLAYERTEAM'] = df_dk['PLAYER']+df_dk['TEAM']       
+    pdf_dk = df_dk[df_dk['POS']=='SP'] # pitchers
+    hdf_dk = df_dk[df_dk['POS']!='SP'] # hitters
+    hdf_dk['BO'] = hdf_dk['TEAM'].cumcount()+1  # make new col 'bat order'
     
-    # create new col Name_Team for merge; 0 is a bad fantasy score to use    
-    pdf1['Name_Team'] = name_proc(pdf1,'Player Name','Team'); pdf1 = pdf1[pdf1['Proj FP']>0]       
-    pdf2['Name_Team'] = name_proc(pdf2,'Player Name','Team'); pdf2 = pdf2[pdf2['Proj FP']>0]
-    hdf1['Name_Team'] = name_proc(hdf1,'Player Name','Team'); hdf1 = hdf1[hdf1['Proj FP']>0]
-    hdf2['Name_Team'] = name_proc(hdf2,'Player Name','Team'); hdf2 = hdf2[hdf2['Proj FP']>0]
+    df_fd = pd.read_csv('in/FD{}.csv'.format(date))  #pitching
+    df_fd = df_fd[['PLAYER','PARTNERID','POS','SALARY','TEAM','OPP','FPTS']]
+    df_fd['PLAYERTEAM'] = df_fd['PLAYER']+df_fd['TEAM']       
+    pdf_fd = df_fd[df_fd['POS']=='P'] # pitchers
+    hdf_fd = df_fd[df_fd['POS']!='P'] # hitters
+    hdf_fd['BO'] = hdf_fd.groupby('TEAM').cumcount()+1  # make new col 'bat order'
 
-    pdf = pd.merge(pdf1, pdf2, how = 'inner', on = 'Name_Team')  # fasten FD/DK together
-    hdf = pd.merge(hdf1, hdf2, how = 'inner', on = 'Name_Team')
+    name_discr([pdf_dk, pdf_fd, hdf_dk, hdf_fd])    # check for name discrepancies
+    pdf = pd.merge(pdf_dk, pdf_fd, how = 'inner', on = 'PLAYERTEAM') # fasten FD/
+    hdf = pd.merge(hdf_dk, hdf_fd, how = 'inner', on = 'PLAYERTEAM') # DK together
     
-    name_discr(pdf1, pdf2, hdf1, hdf2, pdf, hdf)    # check for name discrepancies
     df = pd.concat([pdf, hdf])
 
-    df['Pos_x'] = df['Pos_x'].str.split('/').str[0]#for multi-pos players choose 1st pos
-    df['Pos_y'] = df['Pos_y'].str.split('/').str[0]#for multi-pos players choose 1st pos
-    df['Opp_x'].replace(regex=True, inplace=True, to_replace='@', value=r'') # eliminate '@' from opp col
-    df['Opp_y'].replace(regex=True, inplace=True, to_replace='@', value=r'') # eliminate '@' from opp col
+    # no rows with NaN 
+    df = df[df['POS'].notna()]; df = df[df['SALARY'].notna()]; df = df[df['FPTS'].notna()];
+
+    # under construction: make new method that picks OF by default
+    df['POS_x'] = df['POS_x'].apply(pos_split)#for multi-pos players choose 1st pos
+    df['POS_y'] = df['POS_y'].apply(pos_split)#for multi-pos players choose 1st pos
     
-    if numg == '0': teamlist = team_fromfile(date)  # same day game slate option
-    else: teamlist = team_sampler(df, int(numg))  # get random slate
-    df = df[df['Team_x'].isin(teamlist)]
+    if numg == '0': teamlist = set(df['TEAM_x'].to_list())#same day game slate opt
+    else: 
+        teamlist = team_sampler(df, int(numg))  # get random slate
+        df = df[df['TEAM_x'].isin(teamlist)]
 
     return df, teamlist
 
-def mask_maker(df, teamlist):
+def mask_maker(df, teamlist):           # create masks to be used for opt. constraints
     tothit = 9                          # nine hitters in order
-    sp_ = col_to_npbool(df,'Pos','SP')   # pitcher mask
-    rp_ = col_to_npbool(df,'Pos','RP')
-    p_ = col_to_npbool(df,'Pos','P')    
-    p = np.array([sp_[i]+rp_[i]+p_[i] for i in range(len(sp_))])
+    
+    sp_ = col_to_npbool(df,'POS','SP')   # pitcher mask (DK)
+    p_ = col_to_npbool(df,'POS','P')   # pitcher mask (FD)
 
+    p = np.array([sp_[i]+p_[i] for i in range(len(p_))])   # pitcher mask
     h = np.array([1-p[i] for i in range(len(p))])  # hitter mask
 
-    c = col_to_npbool(df,'Pos','C')
-    b1 = col_to_npbool(df,'Pos','1B')   # infielder mask
-    b2 = col_to_npbool(df,'Pos','2B')   
-    b3 = col_to_npbool(df,'Pos','3B')   
-    ss = col_to_npbool(df,'Pos','SS')   
+    c = col_to_npbool(df,'POS','C')
+    b1 = col_to_npbool(df,'POS','1B')   # infielder mask
+    b2 = col_to_npbool(df,'POS','2B')   
+    b3 = col_to_npbool(df,'POS','3B')   
+    ss = col_to_npbool(df,'POS','SS')   
 
-    of = col_to_npbool(df,'Pos','OF')   # outfielder mask
+    of = col_to_npbool(df,'POS','OF')   # outfielder mask
 
     tb = []               # team masks
     ob = []               # opponent masks
     for team in teamlist:     
-        tb.append(col_to_npbool(df,'Team',team))
-        ob.append(col_to_npbool(df,'Opp',team))                   # team opponent
+        tb.append(col_to_npbool(df,'TEAM',team))
+        ob.append(col_to_npbool(df,'OPP',team))                   # team opponent
         #print(team)
         #print(tb[-1])
     
     st = {2:[], 3: [], 4: [] , 5: []}  # stacking masks
     for n in [2, 3, 4, 5]:  # n is consecutive number of batters
         for k in range(1, tothit+1):   #  for all 9 possible masks
-            stemp = np.array([0]*len(df['Batting Order']))
+            stemp = np.array([0]*len(df['BO']))
             for i in range(n):
-                if k+i > tothit: stemp+=(df['Batting Order']==str(k+i-tothit))
-                if k+i <= tothit: stemp+=(df['Batting Order']==str(k+i))
+                if k+i > tothit: stemp+=(df['BO']==str(k+i-tothit))
+                if k+i <= tothit: stemp+=(df['BO']==str(k+i))
             st[n].append(stemp)
     
     #masks in dict for easier transport 
@@ -170,37 +137,22 @@ def mask_maker(df, teamlist):
 
 def output(rframe, date, rostnum, ng, platform):
     if ng != '0': return     # for random slate, this method not used
-    if rostnum == 1: 
-        os.system('rm -rf platform/*Rosters*{}*'.format(date))  # reset file if first rodeo
-        open('platform/{}Rosters{}.csv'.format('DK', date), 'w').write('P,P,C,1B,2B,3B,SS,OF,OF,OF\n')  # output titles
-        open('platform/{}Rosters{}.csv'.format('FD', date), 'w').write('P,C/1B,2B,3B,SS,OF,OF,OF,UTIL\n') 
-
+    if rostnum == 1: # open output file 
+        os.system('rm -rf out/*Rosters*{}*'.format(date)) # reset if first rodeo
+        open('out/{}Rosters{}.csv'.format('DK', date), 
+             'w').write('P,P,C,1B,2B,3B,SS,OF,OF,OF\n')  # output titles
+        open('out/{}Rosters{}.csv'.format('FD', date),          
+             'w').write('P,C/1B,2B,3B,SS,OF,OF,OF,UTIL\n') 
     filehdl = open('platform/{}Rosters{}.csv'.format(platform, date), 'a')
-    plframe = pd.read_csv('platform/{}Salaries{}.csv'.format(platform, date))  # get todays platform data
-    
-    names = (platform=='DK')*'Name'+(platform=='FD')*'Nickname'  # make new name/team column for matching
-    teams = (platform=='DK')*'TeamAbbrev'+(platform=='FD')*'Team'
-    outid = (platform=='DK')*'ID'+(platform=='FD')*'Id'  # name used for outputfile
-    plframe['Name_Team'] = name_proc(plframe, names, teams)
 
     posdict = {'P':[],'C':[],'1B':[],'2B':[],'3B':[],'SS':[],'OF':[],'UTIL':[]}
     
     for i, row in rframe.iterrows():
-        nametm = row['Name_Team']  
-        match = plframe[plframe['Name_Team'] == nametm]
-        if len(match) == 0:
-            print('Warning: {} not found'.format(nametm))
-        pos = row['Pos']
-        if pos in ['P','RP','SP']: posdict['P'].append(match[outid].values[0])
-        if pos in ['C']: posdict['C'].append(match[outid].values[0])
-        if pos in ['1B']: posdict['1B'].append(match[outid].values[0])
-        if pos in ['2B']: posdict['2B'].append(match[outid].values[0])
-        if pos in ['3B']: posdict['3B'].append(match[outid].values[0])
-        if pos in ['SS']: posdict['SS'].append(match[outid].values[0])
-        if pos in ['OF']: posdict['OF'].append(match[outid].values[0])
+        posdict[row['POS']].append(row['PLAYERTEAM'])
         
-    for strng in ['C','1B','2B','3B','SS']: # under any of these conditions, these are FD results
-        if len(posdict[strng]) > 1:         # therefore, put extra player in UTIL category
+    # these conds indicate FD results, therefore put extra player in UTIL category 
+    for strng in ['C','1B','2B','3B','SS']: 
+        if len(posdict[strng]) > 1: 
            posdict['UTIL'] = [posdict[strng].pop()]
     if len(posdict['OF']) > 3:
            posdict['UTIL'] = [posdict['OF'].pop()]
--- solver.py
+++ solver.py
@@ -2,82 +2,10 @@ import numpy as np , gurobipy as gp, time
 from gurobipy import GRB
 from pyomo.environ import *
     
-def gurobi(df, m, params, limits, rosters, platform):
-    #need to adjust constraints for FD
-    # adjust graming for FD (make sure they all have same indices)
-    #order constraint not working sometimes
-    #SUCCESSive order constraints for when things run out
-    # convert dictionary to actual variable names
-    p, h, teambool, st = m['p'], m['h'], m['tb'], m['st']
-    c, b1, b2, b3, ss, of = m['c'], m['b1'] , m['b2'], m['b3'], m['ss'], m['of']
-    B, stack, overlap = params['B'], params['stack'], params['overlap']
-    nptch, nhit, no_c, no_1b, no_c1b, no_2b = limits['no_ptch'], limits['no_hit'], limits['no_c'], limits['no_1b'], limits['no_c1b'], limits['no_2b']
-    no_3b, no_ss, no_of = limits['no_3b'], limits['no_ss'], limits['no_of']   
-     
-    pr = np.array(df['Proj FP'])
-
-    cost = np.array(df['Salary'].to_list())  #cost vector
-    d = np.array([float(cost[i]) for i in range(len(cost))])
-
-    v1 = np.array(len(df)*[1])  # vector of ones used for summations
-
-    m = gp.Model()
-    m.setParam("LogToConsole",0)
-    x = m.addMVar(len(df), vtype=GRB.BINARY) # roster 
-    v = m.addMVar(len(teambool)*len(st[stack[0]]),vtype=GRB.BINARY)#ensures consecutive
-    #w = m.addMVar(len(teambool)*len(st[stack[1]]),vtype=GRB.BINARY)#hitters
-
-    #Setup Constraints
-    m.addConstr(d @ x <= B)                                  #budget constraint
-
-    m.addConstr(np.diag(p) @ x @ v1 == nptch)           #  num pitchers
-    m.addConstr(np.diag(h) @ x @ v1 == nhit)           #  num hitters
-
-    k = 0  # order counter
-    for tbarr in teambool: 
-        m.addConstr(stack[0]*(np.diag(tbarr) @ np.diag(p) @ x @ v1) + np.diag(tbarr) @ np.diag(h) @ x @ v1 <= stack[0])    # no opposing pitchers / only a max of mst hitters
-        for odx in range(len(st[stack[0]])):          # consecutive batters constraint
-           m.addConstr(np.diag(tbarr) @ np.diag(h) @ np.diag(st[stack[0]][odx]) @ x @ v1 >= stack[0]*v[k])
-           #m.addConstr(np.diag(tbarr) @ np.diag(h) @ np.diag(st[stack[1]][odx]) @ x @ v1 >= stack[1]*w[k])
-           k += 1
-
-    m.addConstr(sum(v) >= 1)    # ensure at least 1 team w order constraint 1
-    #m.addConstr(sum(w) >= 1)    # ensure at least 1 team w order constraint 2
-    #m.addConstr(v @ w == 0)     # make sure order constraints distinct
-           
-    m.addConstr(np.diag(c) @ x  @ v1 + np.diag(b1) @ x  @ v1 >= 1)  # c/1b <=2 (FD)
-    m.addConstr(np.diag(b2) @ x  @ v1 >= 1)   # positional constraints
-    m.addConstr(np.diag(b3) @ x  @ v1 >= 1)   # minimum for DK/FD
-    m.addConstr(np.diag(ss) @ x  @ v1 >= 1)    
-    m.addConstr(np.diag(of) @ x  @ v1 >= 3) 
-
-    m.addConstr(np.diag(c) @ x  @ v1 <= no_c) 
-    m.addConstr(np.diag(b1) @ x  @ v1 <= no_1b)  # positional constraints 
-    m.addConstr(np.diag(b2) @ x  @ v1 <= no_2b) 
-    m.addConstr(np.diag(b3) @ x  @ v1 <= no_3b) 
-    m.addConstr(np.diag(ss) @ x  @ v1 <= no_ss) 
-   
-    m.addConstr(np.diag(of) @ x  @ v1 <= no_of)   #(below: meant for FD but works w DK
-    m.addConstr(np.diag(c) @ x  @ v1 + np.diag(b1) @ x  @ v1 <= no_c1b)  # c/1b <=2 (FD)
-    
-    for sl in rosters:
-        m.addConstr(sl @ x <= overlap)
-
-    m.setObjective(pr @ x, GRB.MAXIMIZE)        #objective function
-    m.optimize()
-
-    try: solution = np.array([int(v.X) for v in m.getVars() if abs(v.obj) > 1e-6])
-    except AttributeError: solution = []
-    return solution
  
-def dctry(frm, lst): return dict(zip(frm['Name_Team'],lst))    
+def dctry(frm, lst): return dict(zip(frm['PLAYERTEAM'],lst))    
 
 def glp(df, m, params, limits, rosters, platform): # GLP solver (uses executable gplsol)
-    #need to adjust constraints for FD
-    # adjust graming for FD (make sure they all have same indices)
-    #order constraint not working sometimes
-    #SUCCESSive order constraints for when things run out
-    # convert dictionary to actual variable names
     p, h, tb, ob, st = dctry(df,m['p']), dctry(df,m['h']), m['tb'], m['ob'], m['st']
     c, b1, b2 = dctry(df,m['c']), dctry(df,m['b1']) , dctry(df,m['b2'])
     b3, ss, of = dctry(df,m['b3']), dctry(df,m['ss']), dctry(df,m['of'])
@@ -85,14 +13,14 @@ def glp(df, m, params, limits, rosters, platform): # GLP solver (uses executable
     nptch, nhit = limits['no_ptch'], limits['no_hit']
     no_c, no_1b, no_c1b, no_2b = limits['no_c'], limits['no_1b'], limits['no_c1b'], limits['no_2b']
     no_3b, no_ss, no_of = limits['no_3b'], limits['no_ss'], limits['no_of']   
-    df_dict = df.set_index('Name_Team').to_dict()    # pyomo likes dictionaries instead of frames
+    df_dict = df.set_index('PLAYERTEAM').to_dict()    # pyomo likes dictionaries instead of frames
 
-    pr = df_dict['Proj FP']
+    pr = df_dict['FPTS']
 
-    d = df_dict['Salary']  #cost vector
+    d = df_dict['SALARY']  #cost vector
 
     model = ConcreteModel()
-    model.ITEMS = df['Name_Team'].to_list()
+    model.ITEMS = df['PLAYERTEAM'].to_list()
     model.ORDERS = range(len(tb)*len(st[stack[0]]))
     model.x = Var( model.ITEMS, within=Binary )
     model.v = Var(model.ORDERS, within=Binary)#ensures consecutive
